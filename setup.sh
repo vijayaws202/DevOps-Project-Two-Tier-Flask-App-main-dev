febf6ba5ca1697781a730e5b0ca6a6e57b654652
vijaydudhamal <165992267+vijaydudhamal@users.noreply.github.com>
2025-11-17 15:55:39 +0530

    add source code to dev repo

---
+++ setup.sh
@@ -0,0 +1,68 @@
+
+# **Update System Packages:**
+sudo apt update && sudo apt upgrade -y
+
+# Install Git, Docker, and Docker Compose:**
+    
+    sudo apt install git docker.io docker-compose-v2 -y
+
+# **Start and Enable Docker:**
+    ```bash
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    ```
+
+**Add User to Docker Group (to run docker without sudo):**
+    ```bash
+    sudo usermod -aG docker $USER
+    newgrp docker
+    ```    
+
+**5. Step 3: Jenkins Installation and Setup**
+
+ # **Install Java (OpenJDK 17):**
+    ```bash
+    sudo apt install openjdk-17-jdk -y
+
+
+    ```
+ # **Add Jenkins Repository and Install:**
+    ```bash
+    sudo apt install curl -y 
+    curl -fsSL [https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key](https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key) | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] [https://pkg.jenkins.io/debian-stable](https://pkg.jenkins.io/debian-stable) binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+    sudo apt update
+    sudo apt install jenkins -y
+    ```
+# ** Add GPG key
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+ # Add jenkins Repo 
+
+ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/" | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+ # Update and install jenkins
+
+ sudo apt-get update
+sudo apt-get install jenkins -y
+
+# Start & enable the jenkins service
+
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+# **Initial Jenkins Setup:**
+# * Retrieve the initial admin password:
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+# see like this on terminal: 8fd91619fc7840fc9c09064795f5f1d8
+
+# **Grant Jenkins Docker Permissions:**
+    ```bash
+    sudo usermod -aG docker jenkins
+    sudo systemctl restart jenkins
+    ```
